98c6a2db32c443d64585cc22d73cdb968f4a0717
cathio-dev <ramalho.marcos7@gmail.com>
2024-11-04 23:20:32 -0300

    modificação josephus

--- AtvLSE/1030_JosephusSuicida.c
+++ AtvLSE/1030_JosephusSuicida.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include "listase.h"
 
-int FlaviousJosephus(tp_listase **plista, int n, int k){
+int FlaviousJosephus(tp_listase **plista/*, int n, int k*/){
     tp_listase *atu, *ant;
     int pessoa;
 
-    for(int i = 0; i < n; i++){ //2, 3, 4, 5, 1 . TESTE
+    /*for(int i = 0; i < n; i++){ //2, 3, 4, 5, 1 . TESTE
         printf("Digite o %d numero: ", i + 1);
         scanf("%d", &pessoa);
         insere_listase_no_fim(plista, pessoa); //DUVIDA, plista já é ponteiro de ponteiro, e na funcao insere passamos o endereço do ponteiro, mas como plista ja tem um endereço do ponteiro, passamos só a PLISTA.
-    }
+    }*/
+
+    int k = 2;
+    insere_listase_no_fim(plista, 2);
+    insere_listase_no_fim(plista, 3);
+    insere_listase_no_fim(plista, 4);
+    insere_listase_no_fim(plista, 5);
+    insere_listase_no_fim(plista, 1);
+
 
     //primeira parte feita, preencher a lista.
     ///////////////////////////////////////////////
     atu = *plista;
     ant = NULL;
 
-    while(atu-> prox != NULL){
+    
+    while((atu->prox != *plista) && (atu-> prox != NULL)){
         atu = atu -> prox; //quando prox for NULL, ele volta para o ultimo prox.
     }
 
@@ -45,14 +54,14 @@ int main(){
     CirandaSuicidio = inicializa_listase();
     int N, GAP;
 
-    printf("Selecione o numero de pessoas: ");
+    /*printf("Selecione o numero de pessoas: ");
     scanf("%d", &N);
 
     printf("\nSelecione o numero de saltos entre os suicídios: ");
     scanf("%d", &GAP);
-    printf("\n");
+    printf("\n");*/
 
-    FlaviousJosephus(&CirandaSuicidio, N, GAP);
+    FlaviousJosephus(&CirandaSuicidio/*, N, GAP*/);
 
 
 
